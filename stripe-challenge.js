66249719743d9f28c175e5a1e901517091c9f480
nurbek82 <nurbek82@gmail.com>
2020-07-17 11:42:16 -0700

    This is how we invoke the plugin
    The function has been stored in $.fn now, instead of our Globals object. Similarly, near the end, we store our default color in $.fn.stripe.
    
    The first parameter has been removed, since that will now be the query upon which our plugin acts. Because of that, within the code, the $(query) has been replaced with this.
    
    Before we start finding tables, we override the defaults with any incoming options.
    
    To properly behave as a jQuery collection method, we return this at the end.

---
+++ stripe-challenge.js
@@ -0,0 +1,37 @@
+(function($){
+	$.fn.stripe = function(options) {
+
+		// override defaults with options
+		var opts = $.extend({}, $.fn.stripe.defaults, options);
+
+		// start with empty set
+		var $tables = $()
+			// add any tables that are direct entries in current collection
+			.add(this.filter('table').get())
+			// add ones that are descendants of entries in current collection
+			.add(this.find('table').get());
+		// now find any tables that are descendants of the tables we found
+		var $omit = $tables.find('table');
+		// and omit them from the collection
+		$tables = $tables.not($omit);
+
+		var oddColor = opts.oddColor ? opts.oddColor : null;
+		var evenColor = opts.evenColor ? opts.evenColor : null;
+
+		$tables.each(function() {
+				// this finds only grandchildren (table -> table section -> tr)
+				if (opts.tbodyOnly) var $sections = $(this).children('tbody');
+				else var $sections = $(this).children();
+				if (oddColor) $sections.children('tr:odd')
+					.css('backgroundColor', oddColor);
+				if (evenColor) $sections.children('tr:even')
+					.css('backgroundColor', evenColor);
+		});
+		return this;
+	};
+
+	$.fn.stripe.defaults = {
+		evenColor: '#ddddff',
+		tbodyOnly: true
+	}
+}(jQuery));
