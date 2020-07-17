5deccc68942d0d5d0c15b83d29aac63f4bccf9f3
nurbek82 <nurbek82@gmail.com>
2020-07-17 11:38:53 -0700

    finished the jQuery Plugin

--- stripe.js
+++ stripe.js
@@ -1,28 +1,37 @@
-var Globals = {
-	evenColor: "#ddddff"
-};
 (function($){
-	Globals.stripe = function(query, evenColor, oddColor) {		
+
+	$.fn.stripe = function(options) {		
+
+		// override defaults with options
+		var opts = $.extend({}, $.fn.stripe.defaults, options);
+
 		// start with empty set
 		var $tables = $()
 			// add any tables that are direct entries in current collection
-			.add($(query).filter('table').get())
+			.add(this.filter('table').get())
 			// add ones that are descendants of entries in current collection
-			.add($(query).find('table').get());
+			.add(this.find('table').get());
 		// now find any tables that are descendants of the tables we found
 		var $omit = $tables.find('table');
 		// and omit them from the collection
 		$tables = $tables.not($omit);
-		
+
+		var oddColor = opts.oddColor ? opts.oddColor : null;
+		var evenColor = opts.evenColor ? opts.evenColor : null;
+
 		$tables.each(function() {
 				// this finds only grandchildren (table -> table section -> tr)
 				var $sections = $(this).children();
-				var oddClr = oddColor || Globals.oddColor;
-				if (oddClr) $sections.children('tr:odd')
-					.css('backgroundColor', oddClr);
-				var evenClr = evenColor || Globals.evenColor;
-				if (evenClr) $sections.children('tr:even')
-					.css('backgroundColor', evenClr);
-		});		
+				if (oddColor) $sections.children('tr:odd')
+					.css('backgroundColor', oddColor);
+				if (evenColor) $sections.children('tr:even')
+					.css('backgroundColor', evenColor);
+		});
+		return this;
 	};
+
+	$.fn.stripe.defaults = {
+		evenColor: '#ddddff'
+	}
+
 }(jQuery));
